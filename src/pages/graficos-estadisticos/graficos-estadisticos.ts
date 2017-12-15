74ccad327b1db550e608d495d8659e0f5495e0ee
Nico Franzese <franzesens@gmail.com>
2017-12-15 19:48:32 -0300

    Se agregan estilos, filtros, funciones para los gráficos estadisticos

--- src/pages/graficos-estadisticos/graficos-estadisticos.ts
+++ src/pages/graficos-estadisticos/graficos-estadisticos.ts
@@ -31,11 +31,15 @@ export class GraficosEstadisticosPage {
   public cursos;
   public curso;
   public CantidadPersonas;
+  public encuesta;
+  public grafico;
+  public encuestas;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController,
     private dataProvider: DataProvider) {
     this.CargaMaterias();
     this.obtenerCantidadPersonas();
+    this.obtenerEncuestas();
     this.curso = 'Seleccione Curso'
   }
 
@@ -49,6 +53,18 @@ export class GraficosEstadisticosPage {
       data =>
       {
         this.CantidadPersonas = data.length;
+
+      },
+      err => console.error(err)
+    );
+  }
+
+  private obtenerEncuestas()
+  {
+    this.dataProvider.getItems('cuestionarios').subscribe(
+      data =>
+      {
+        this.encuestas = data;
 
       },
       err => console.error(err)
@@ -81,115 +97,69 @@ export class GraficosEstadisticosPage {
     console.log(this.curso);
   }
 
-  CargarGraficosAsistencias(){
-    if (this.tipoGrafico != "seleccioneTipo"){
-    this.barChart = new Chart(this.barCanvas.nativeElement, {
-      type: 'bar',
-      data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero:true
-                  }
-              }]
-          }
-      }
+    CargaGraficos(){
+        if ((this.tipoGrafico != "seleccioneTipo") && (this.grafico = "Asistencias")){
+            this.barChart = new Chart(this.barCanvas.nativeElement, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
 
-    });
+            });
+        }
+
+
+        if ((this.encuesta != "seleccioneEncuesta") && (this.grafico = "Encuestas")){
+            this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {      
+                type: 'doughnut',
+                data: {
+                    labels: ["Gente que Respondió", "Gente que no respondió"],
+                    datasets: [{
+                        label: '# de gente que realizó encuesta seleccionada',
+                        data: [12, 19],
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)'
+                        ],
+                        hoverBackgroundColor: [
+                            "#FF6384",
+                            "#36A2EB"
+                        ]
+                    }]
+                }
+            });        
+        }
     }
-
-
-
-    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
-      
-                 type: 'doughnut',
-                 data: {
-                     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                     datasets: [{
-                         label: '# of Votes',
-                         data: [12, 19, 3, 5, 2, 3],
-                         backgroundColor: [
-                             'rgba(255, 99, 132, 0.2)',
-                             'rgba(54, 162, 235, 0.2)',
-                             'rgba(255, 206, 86, 0.2)',
-                             'rgba(75, 192, 192, 0.2)',
-                             'rgba(153, 102, 255, 0.2)',
-                             'rgba(255, 159, 64, 0.2)'
-                         ],
-                         hoverBackgroundColor: [
-                             "#FF6384",
-                             "#36A2EB",
-                             "#FFCE56",
-                             "#FF6384",
-                             "#36A2EB",
-                             "#FFCE56"
-                         ]
-                     }]
-                 }
-      
-             });
-      
-             this.lineChart = new Chart(this.lineCanvas.nativeElement, {
-      
-                 type: 'line',
-                 data: {
-                     labels: ["January", "February", "March", "April", "May", "June", "July"],
-                     datasets: [
-                         {
-                             label: "My First dataset",
-                             fill: false,
-                             lineTension: 0.1,
-                             backgroundColor: "rgba(75,192,192,0.4)",
-                             borderColor: "rgba(75,192,192,1)",
-                             borderCapStyle: 'butt',
-                             borderDash: [],
-                             borderDashOffset: 0.0,
-                             borderJoinStyle: 'miter',
-                             pointBorderColor: "rgba(75,192,192,1)",
-                             pointBackgroundColor: "#fff",
-                             pointBorderWidth: 1,
-                             pointHoverRadius: 5,
-                             pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                             pointHoverBorderColor: "rgba(220,220,220,1)",
-                             pointHoverBorderWidth: 2,
-                             pointRadius: 1,
-                             pointHitRadius: 10,
-                             data: [65, 59, 80, 81, 56, 55, 40],
-                             spanGaps: false,
-                         }
-                     ]
-                 }
-      
-             });
-
-
-  }
-
-
 
 }
 
